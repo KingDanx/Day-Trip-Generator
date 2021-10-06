a7ce75b6ea82e1069423393f9ff489e9249fcf9e
Dan Prudhomme <dannyprud@yahoo.com>
2021-10-05 20:15:55 -0500

    Added several arrays to the project

---
+++ day-trip-gen.js
@@ -0,0 +1,18 @@
+//Dan Prudhomme
+"use strict";
+//(5 points): As a developer, I want to make at least three commits with descriptive messages. -- count 1
+
+//(5 points): As a user, I want a destination to be randomly selected for my day trip.
+let randomDestination = ["Hawaii", "Tennessee", "Florida", "Tokyo", "Perth"];
+
+//(5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+let randomFood = ["Foul", "Fish", "Salad", "Pork", "Beef", "CANDY"];
+
+//(5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+let randomTransportation = ["Car", "Bus", "Train", "Horse", "Walk", "Run", "Ship", "Falcon X Rocet"];
+
+//(5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+let randomEntertainment = ["Theme Park", "See the sights", "Shopping Center", "Beach", "Local Museum"];
+
+/*(15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form 
+of entertainment if I don’t like one or more of those things.*/
